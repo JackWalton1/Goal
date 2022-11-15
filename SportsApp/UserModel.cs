20d732729ffbefcee3e2be3d8bdb2ee0cc9965e5
JackWalton1 <jwalto02@calpoly.edu>
2022-11-15 16:12:02 +0100

    fixed null values and added locationString

--- SportsApp/UserModel.cs
+++ SportsApp/UserModel.cs
@@ -1,14 +1,20 @@
-﻿using MongoDB.Bson;
+﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace SportsApp;
 
 public class UserModel
 {
-	[BsonId]
+    [Required]
+    [BsonId]
 	[BsonRepresentation(BsonType.ObjectId)]
 	public string Id { get; set; }
-	public string Username { get; set; }
-	public string Password { get; set; }
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    public string Password { get; set; }
+    [Required]
+    public string LocationString { get; set; }
 }
 
